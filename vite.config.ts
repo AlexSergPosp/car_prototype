99f8d72651680992b5487cc1072e99e465e9b3a9
apospelov <alex.serg.posp@gmail.com>
2026-07-06 22:23:59 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const githubRepositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "car_prototype";
+
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/busines/" : "/",
+  base: process.env.GITHUB_ACTIONS ? `/${githubRepositoryName}/` : "/",
   plugins: [react(), tailwindcss()],
 });
